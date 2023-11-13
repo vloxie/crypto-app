a8db72ec9cae1f7af8a5127b33754815b41cc29f
vloxie <neil.aldred@hotmail.co.uk>
2023-11-13 08:36:19 +0000

    Add API and web routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\FindCrypto;
+use App\Http\Controllers\API\ListCrypto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('list', ListCrypto::class)->name('crypto.list');
+Route::get('find/{crypto}', FindCrypto::class)->name('crypto.find');
